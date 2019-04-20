56333e0e81d70488fb13341c378eea72b64619c5
BoomMan <l.o.v.e_y.o.u_3344@qq.com>
2019-04-20 22:40:13 +0800

    增加wechat模块

---
+++ wechat/src/main/java/cn/boommanpro/WechatApplication.java
@@ -0,0 +1,10 @@
+package cn.boommanpro;
+
+import cn.boommanpro.wechat.annotation.EnableWeChat;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableWeChat
+@SpringBootApplication
+public class WechatApplication {
+
+}
