4db116d8c98518ed056ca632566cb72ef7a42171
BoomMan <l.o.v.e_y.o.u_3344@qq.com>
2019-04-21 19:31:47 +0800

    修改servlet作用域和 web模块新增启动提示

--- web/src/main/java/cn/boommanpro/WebApplication.java
+++ web/src/main/java/cn/boommanpro/WebApplication.java
@@ -2,13 +2,28 @@ package cn.boommanpro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @EnableScheduling
 @SpringBootApplication
 public class WebApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = new SpringApplication(WebApplication.class).run();
+
+        try {
+            String host = InetAddress.getLocalHost().getHostAddress();
+            TomcatServletWebServerFactory tomcatServletWebServerFactory = (TomcatServletWebServerFactory) context.getBean("tomcatServletWebServerFactory");
+            int port = tomcatServletWebServerFactory.getPort();
+            String contextPath = tomcatServletWebServerFactory.getContextPath();
+            String path = "http://" + host + ":" + port + contextPath + "/";
+            System.out.println("<------------------------------------------  " + path + "  ------------------------------------------>");
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
     }
 }
